7dc9086ad6572d949a99699ea6e866d161c7d7db
TheSmallPixel <lorenzolongiave@hotmail.com>
2019-03-24 13:39:34 +0100

    Update Auth.cs

--- GiupiterWebApi/Data/Auth.cs
+++ GiupiterWebApi/Data/Auth.cs
@@ -7,9 +7,9 @@ namespace GiupiterWebApi.Data
 {
     public static class Auth
     {
-        public static string accountSid = "ACb14ad05950039635eb6a1b3f72956302";
-        public static string authToken = "5659349c13759b580cc0ddad7bf5cd93";
-        public static string Number = "390294751616";
-        public static string UrlPath = "http://193.70.39.16:80";
+        public static string accountSid = "**************************";
+        public static string authToken = "*******************************";
+        public static string Number = "******************";
+        public static string UrlPath = "*********************";
     }
 }
